f95c35558aa3d57955982f52c57745a97ffb5df6
SK Kim <a01055756383@gmail.com>
2026-08-12 16:13:04 +0900

    button_add&sw_debounce

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,8 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_gpio.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -34,12 +36,18 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 #define RCC_AHB1ENR (*(volatile unsigned int*)(0x40023800 + 0x30))
+
+
 #define GPIOA_MODER (*(volatile unsigned int*)(0x40020000 + 0x00))
 #define GPIOA_OTYPER (*(volatile unsigned int*)(0x40020000 + 0x04))
 
 //GPIOB
 #define GPIOB_MODER (*(volatile unsigned int*)(0x40020400 + 0x00))
 #define GPIOB_OTYPER (*(volatile unsigned int*)(0x40020400 + 0x04))
+
+// ODR
+#define GPIA_ODR (*(volatile unsigned int*) 0x40020014)
+#define GPIB_ODR (*(volatile unsigned int*) 0x40020414)
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -92,39 +100,39 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
-  // MX_GPIO_Init();
-  // MX_USART2_UART_Init();
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
   // *(volatile unsigned int*) 0x40020000 = (2u<<10);
   // GPIOA 클럭 활성화
-  RCC_AHB1ENR |= (1u<<0);
-  // *(volatile unsigned int*)(0x40023800 + 0x30) |= (1u<<0);
-  // GPIOA_MODER
-  GPIOA_MODER &=~(3u<<10);
-  GPIOA_MODER |= (1u<<10);
-  // *(volatile unsigned int*)(0x40020000 + 0x00) &=~(3u<<10);
-  // *(volatile unsigned int*)(0x40020000 + 0x00) |= (1u<<10);
+  // RCC_AHB1ENR |= (1u<<0);
+  // // *(volatile unsigned int*)(0x40023800 + 0x30) |= (1u<<0);
+  // // GPIOA_MODER
+  // GPIOA_MODER &=~(3u<<10);
+  // GPIOA_MODER |= (1u<<10);
+  // // *(volatile unsigned int*)(0x40020000 + 0x00) &=~(3u<<10);
+  // // *(volatile unsigned int*)(0x40020000 + 0x00) |= (1u<<10);
 
-  // push pull
-  GPIOA_OTYPER &=~(1u<<5);
-  // *(volatile unsigned int*)(0x40020000 + 0x04) &=~(1u<<5);
+  // // push pull
+  // GPIOA_OTYPER &=~(1u<<5);
+  // // *(volatile unsigned int*)(0x40020000 + 0x04) &=~(1u<<5);
 
 
-  // PB0 클럭 인가방법
-  // 0번핀 출력으로 만드는법
-  // PB0 에 대해서 RCC설정, MODER설정, PUSHPULL설정, toggle프로그램 추가
-  // GPIOB 주소 시작위치 0x4002 0400
-  // 클럭 0x30 B0 위치 0
+  // // PB0 클럭 인가방법
+  // // 0번핀 출력으로 만드는법
+  // // PB0 에 대해서 RCC설정, MODER설정, PUSHPULL설정, toggle프로그램 추가
+  // // GPIOB 주소 시작위치 0x4002 0400
+  // // 클럭 0x30 B0 위치 0
 
-  // GPIOB 클럭 활성화 -> RCC_AHB1ENR의 주소에 1을 왼쪽으로 1만큼 움직여서 1로 변환
-  RCC_AHB1ENR |= (1u<<1);
+  // // GPIOB 클럭 활성화 -> RCC_AHB1ENR의 주소에 1을 왼쪽으로 1만큼 움직여서 1로 변환
+  // RCC_AHB1ENR |= (1u<<1);
 
-  // GPIOB_MODER -> GPIOB_MODER주소에있는 값을 0으로 초기화 후 01로 만들어서 활성화
-  GPIOB_MODER &=~(3u<<0);
-  GPIOB_MODER |= (1u<<0);
+  // // GPIOB_MODER -> GPIOB_MODER주소에있는 값을 0으로 초기화 후 01로 만들어서 활성화
+  // GPIOB_MODER &=~(3u<<0);
+  // GPIOB_MODER |= (1u<<0);
 
-  // push pull
-  GPIOB_OTYPER &=~(1u<<0);
+  // // push pull
+  // GPIOB_OTYPER &=~(1u<<0);
 
 
 
@@ -136,14 +144,24 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  // HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-	  // HAL_Delay(500);
-    *(volatile unsigned int*) 0x40020014 ^= (1u<<5);
-    *(volatile unsigned int*) 0x40020414 ^= (1u<<0);
+    // GPIA_ODR ^= (1u<<5);
+    // GPIB_ODR ^= (1u<<0);
+    
+    // volatile int delay_count = 1000000;
+    // while(delay_count--) { }
+    GPIO_PinState button = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13);
+    if (button == GPIO_PIN_RESET) {
+      HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);  
+    }
 
-
-    volatile int delay_count = 1000000;
-    while(delay_count--) { }
+    GPIO_PinState button1 = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_0);
+    if (button1 == GPIO_PIN_RESET) {
+      HAL_Delay(50);
+      button1 = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_0);
+      if (button1 == GPIO_PIN_RESET) {
+        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+      }
+    }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
