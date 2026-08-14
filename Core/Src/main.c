3b8b56d25dfbefc1d1fe211ed2a5ff9b8af33dc9
SK Kim <a01055756383@gmail.com>
2026-08-14 16:24:55 +0900

    0814_liveChat_realTime

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -27,22 +27,25 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+/* Standard I/O Re-targeting for printf() */
 #ifdef __GNUC__
   #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
 #else
   #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
 #endif
-
 PUTCHAR_PROTOTYPE
 {
   /* Polling 방식으로 1바이트 전송 (전송 완료될 때까지 대기) */
   HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
   return ch;
 }
+
 #define RX_BUF_SIZE 128
-uint8_t rx_data;
 uint8_t rx_buf1[RX_BUF_SIZE];
 uint8_t rx_buf2[RX_BUF_SIZE];
+uint8_t rx_data2;
+uint16_t rx2_idx;
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -74,60 +77,66 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
-
-  if(GPIO_Pin==GPIO_PIN_13){
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+// void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+//   if(GPIO_Pin==GPIO_PIN_13){
+//     HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+//   }
+//   if(GPIO_Pin==GPIO_PIN_0){
+//     HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+//   }
+// }
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+  if(huart->Instance == USART2) {
+    if(rx_data2 == '\b')
+    {
+      if (rx2_idx > 0) {
+        rx2_idx--; // 버퍼 커서를 한 칸 뒤로
+        rx_buf2[rx2_idx] = 0; // 지우기
+        //나의 터미널에도 백스페이스 적용
+        HAL_UART_Transmit(&huart2, (uint8_t*)"\b \b", 3, 10);
+      }
+    }
+    else if(rx_data2 == '\r' || rx_data2 == '\n')
+    {
+      rx_buf2[rx2_idx] = '\r';
+      rx2_idx++;
+      rx_buf2[rx2_idx] = '\n';
+      rx2_idx++;
+      HAL_UART_Transmit(&huart1, rx_buf2,rx2_idx,100);
+      //나의 터미널에 엔터 적용
+      HAL_UART_Transmit(&huart2, (uint8_t*)"\r\n",2,100);
+      memset(rx_buf2,0,rx2_idx);
+      rx2_idx = 0;
+      HAL_UARTEx_ReceiveToIdle_DMA( &huart1, rx_buf1,RX_BUF_SIZE);
+    }
+    else{
+      //나의 터미널에 문자 출력
+      if (rx2_idx < RX_BUF_SIZE - 1) { // 버퍼 오버플로우 방지
+        rx_buf2[rx2_idx++] = rx_data2;
+        HAL_UART_Transmit(&huart2, &rx_data2, 1, 10);
+      }
+    }
+    // 다음 1바이트 수신 대기
+    HAL_UART_Receive_IT(&huart2, &rx_data2, 1);
   }
 }
-
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
-
-  if(huart->Instance==USART2){
-    if(rx_data=='a')
-      printf("Hello STM32 Cortex-M4 USART Polling!\r\n");
-    else
-      HAL_UART_Transmit(&huart2, rx_buf2, RX_BUF_SIZE, 100);
-
-    HAL_UARTEx_ReceiveToIdle_DMA(&huart2, rx_buf2, RX_BUF_SIZE);
-  }
-}
-
-
-void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
-{
-  if(huart->Instance==USART1){ 
-    if(rx_data=='a')
-      printf("Hello STM32 Cortex-M4 USART Polling!\r\n");
-    else
-      HAL_UART_Transmit(&huart2, rx_buf1, Size, 100); //2로 넘어와?
-
-    HAL_UART_DMAStop(&huart1);
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size){
+  if(huart->Instance == USART1){
+    HAL_UART_Transmit(&huart2, rx_buf1,Size,100);
     memset(rx_buf1,0,Size);
-    
+    HAL_UARTEx_ReceiveToIdle_DMA( &huart1, rx_buf1,RX_BUF_SIZE);
   }
-  
-  if (huart->Instance==USART2){ 
-    if(rx_data=='a')
-    printf("Hello STM32 Cortex-M4 USART Polling!\r\n");
-  else
-  HAL_UART_Transmit(&huart1, rx_buf2, Size, 100);
-
-  HAL_UART_DMAStop(&huart2);
-  memset(rx_buf2,0,Size);
-  }
-  HAL_UARTEx_ReceiveToIdle_DMA(&huart1, rx_buf1, RX_BUF_SIZE);
-  HAL_UARTEx_ReceiveToIdle_DMA(&huart2, rx_buf2, RX_BUF_SIZE);
+  // if(huart->Instance == USART2){
+  //   HAL_UART_Transmit(&huart1, rx_buf2,Size,100);
+  //   HAL_UART_DMAStop(&huart2);
+  //   memset(rx_buf2,0,Size);
+  //   HAL_UARTEx_ReceiveToIdle_DMA( &huart2, rx_buf2,RX_BUF_SIZE);
+  // }
 }
 
 void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart){
-  HAL_UART_Receive_DMA(&huart2, rx_buf1, RX_BUF_SIZE);
-  HAL_UART_Receive_DMA(&huart2, rx_buf2, RX_BUF_SIZE);
+  HAL_UARTEx_ReceiveToIdle_DMA( &huart1, rx_buf1,RX_BUF_SIZE);
 }
-
-
-
 /* USER CODE END 0 */
 
 /**
@@ -163,17 +172,19 @@ int main(void)
   MX_USART2_UART_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
- //HAL_UART_Receive_IT(&huart2, &rx_data, 1);
-  HAL_UARTEx_ReceiveToIdle_DMA(&huart2, rx_buf2, RX_BUF_SIZE);
-  HAL_UARTEx_ReceiveToIdle_DMA(&huart1, rx_buf1, RX_BUF_SIZE);
-
+  HAL_UARTEx_ReceiveToIdle_DMA(&huart1,rx_buf1, RX_BUF_SIZE);
+  HAL_UART_Receive_IT(&huart2, &rx_data2, 1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-
+    //
+    // if(HAL_UART_Receive(&huart2, &rx_data, 1, 10) == HAL_OK){
+    /* Polling 방식으로 1초마다 콘솔에 문자열 출력 */
+    // printf("Hello STM32 Cortex-M4 USART Polling!\r\n");
+    //HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -261,3 +272,4 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
+
