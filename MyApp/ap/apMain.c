f24c39b64c0189aa7c4138529346e26e327ead19
SK Kim <a01055756383@gmail.com>
2026-08-19 16:35:45 +0900

    0819_dht11

--- MyApp/ap/apMain.c
+++ MyApp/ap/apMain.c
@@ -2,6 +2,7 @@
 #include "myAdc.h"
 #include "myUart.h"
 #include "myTim.h"
+#include "myDht11.h"
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx_hal_adc.h"
 
@@ -16,21 +17,25 @@ extern ADC_HandleTypeDef hadc1;
 void apInit(void) { 
   // uartInit();
   // adcInit();
-  myTim_Init();
+  // myTim_Init();
+  dht11Init();
 }
 
 float internal_temp =0;
+dht11Data_t dht_data = {0};
+bool dht_status = false;
 
 void apMain(void) {
 
   while (1) {
-    HCSR04_Trigger();
+    // adcUpdate();
 
-    HAL_Delay(60);
-
-    printf(">distance : %.2f\r\n", distance);
-    printf("distance : %.2f cm\r\n", distance);
-
-    // HAL_Delay(1000);
+    dht_status = dht11Read(&dht_data);
+    
+    // HCSR04_Trigger();
+    // printf(">distance : %.2f\r\n", distance);
+    // printf("distance : %.2f cm\r\n", distance);
+    
+    HAL_Delay(10);
   }
 }
